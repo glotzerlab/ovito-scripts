647b85eeb1260f48aa27730eec48e861752680f3
Bradley Dice <bdice@bradleydice.com>
2020-01-10 23:16:26 -0500

    Add license header.

--- freud/ovito_EnvironmentCluster.py
+++ freud/ovito_EnvironmentCluster.py
@@ -1,4 +1,6 @@
-from ovito.data import *
+# Copyright (c) 2019 The Regents of the University of Michigan
+# All rights reserved.
+# This software is licensed under the BSD 3-Clause License.
 
 import freud
 
