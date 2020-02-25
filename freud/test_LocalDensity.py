58494328428470ed7ad83de68c3bec201db603d8
Michael Stryk <mstryk@umich.edu>
2020-02-25 13:32:38 -0500

    Updated test_LocalDensity (removed input,output) changed to data type: data

--- freud/test_LocalDensity.py
+++ freud/test_LocalDensity.py
@@ -5,26 +5,22 @@
 import freud
 from ovito.data import *
 
-def sense_check(r_max, diameter, dimensions):
-    if r_max==0 or diameter==0 or dimensions==0:
+def sense_check(r_max, diameter):
+    if r_max==0 or diameter==0:
         print('WARNING! OVERWRITE VALUES IN SCRIPT')
     return
 
-def modify(frame, input, output):
+def modify(frame, data):
     #   USER SHOULD SET THESE MANUALLY BEFORE RUNNING SCRIPT
-    dimensions = 2
-    r_max = 0
-    diameter = 0
-    sense_check(r_max, diameter, dimensions)
-    
-    if input.particles is not None:
-        box = freud.box.Box.from_matrix(input.cell.matrix, dimensions)
-        points = input.particles.position
-        system = (box, points)
-        print(system)
-        # can we automatically detect particle circumsphere diameter?
+    r_max = 3
+    diameter = 0.05
+    sense_check(r_max, diameter)
 
+    if input.particles is not None:
+        box = freud.box.Box.from_matrix(data.cell.matrix)
+        points = data.particles.positions
+        system = (box, points)
         ld = freud.density.LocalDensity(r_max,diameter)
-        ld.compute(system, input.particles.position)
-        output.create_user_particle_property(name='LocalDensity', data_type=float, data=ld.density)
-        print('Created property for {} particles.'.format(input.particles.count))
+        ld.compute(system)
+        data.create_user_particle_property(name='LocalDensity', data_type=float, data=ld.density)
+        print('Created property for {} particles.'.format(data.particles.count))
