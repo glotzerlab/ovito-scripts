9e19db90376de5295be405ab3fe4773775780797
Michael Stryk <mstryk@umich.edu>
2020-02-25 13:39:02 -0500

    Updated test_Nematic (removed input,output) changed to data

--- freud/test_Nematic.py
+++ freud/test_Nematic.py
@@ -1,5 +1,3 @@
-# Copyright (c) 2019 The Regents of the University of Michigan
-# All rights reserved.
 # This software is licensed under the BSD 3-Clause License.
 
 import freud
@@ -10,17 +8,16 @@ def sense_check(director):
     if director==[]: print('WARNING! OVERWRITE DIRECTOR VALUE IN SCRIPT')
     return
 
-def modify(frame, input, output):
+def modify(frame, data):
     #   USER SHOULD SET THIS MANUALLY BEFORE RUNNING SCRIPT
     director = np.array([1,1,0])
     sense_check(director)
-    
-    if input.particles is not None:
-        box = freud.box.Box.from_matrix(input.cell.matrix, dimensions=2)
-        points = input.particles.position
-        system = (box, points)
 
+    if data.particles is not None:
+        box = freud.box.Box.from_matrix(data.cell.matrix)
+        points = data.particles.positions
+        system = (box, points)
         order_param = freud.order.Nematic(director)
-        order_param.compute(input.particles.orientations)
-        output.create_user_particle_property(name='NematicOrderParameter', data_type=float, data=order_param.particle_tensor)
-        print('Created property for {} particles.'.format(input.particles.count))
+        order_param.compute(points.orientations)  # Hasn't been tested.
+        data.create_user_particle_property(name='NematicOrderParameter', data_type=float, data=order_param.particle_tensor)
+        print('Created property for {} particles.'.format(data.particles.count))
