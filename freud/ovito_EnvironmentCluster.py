5a2a030f804027c75b12dacf0c02953b6534258f
Michael Stryk <mstryk@umich.edu>
2020-02-27 12:58:11 -0500

    removed input/output, changed to data, changed env_cluster.compute

--- freud/ovito_EnvironmentCluster.py
+++ freud/ovito_EnvironmentCluster.py
@@ -4,18 +4,21 @@
 
 import freud
 
-def modify(frame, input, output):
-    
+def modify(frame,data):
     r_max = 1.5
     num_neighbors = 12
     threshold = 0.45 * r_max
     registration = False  # Very slow if enabled!
     global_search = False  # Very slow if enabled!
-    
-    if input.particles is not None:
+    box = freud.box.Box.from_matrix(data.cell.matrix, dimensions=2)
+    points = data.particles.positions
+    system = (box, points)
+
+
+    if data.particles is not None:
         env_cluster = freud.environment.EnvironmentCluster()
-        env_cluster.compute(input, threshold=threshold,
-			    neighbors={'num_neighbors': num_neighbors},
-                            registration=registration, global_search=global_search)
-        output.create_user_particle_property(name='EnvironmentCluster', data_type=int, data=env_cluster.cluster_idx)
-        print('Created property for {} particles.'.format(input.particles.count))
+        env_cluster.compute(system, threshold=threshold,
+                neighbors={'num_neighbors': num_neighbors},
+                registration=registration, global_search=global_search) # Causes script to fail
+        data.create_user_particle_property(name='EnvironmentCluster', data_type=int, data=env_cluster.cluster_idx)
+        print('Created property for {} particles.'.format(data.particles.count))
