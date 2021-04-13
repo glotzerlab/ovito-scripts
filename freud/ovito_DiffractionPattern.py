1510e57c4c12be4adc7bdcf6a38d680af7733b9c
Bradley Dice <bdice@bradleydice.com>
2021-04-13 16:59:16 -0500

    Add DiffractionPattern script compatible with freud 2.5.0.

---
+++ freud/ovito_DiffractionPattern.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2021 The Regents of the University of Michigan
+# All rights reserved.
+# This software is licensed under the BSD 3-Clause License.
+
+import numpy as np
+import PySide2.QtGui
+import rowan
+
+import freud
+
+print("Diffraction, freud version", freud.__version__)
+
+
+def render(args):
+    pipeline = args.scene.selected_pipeline
+    if not pipeline:
+        return
+    data = pipeline.compute(args.frame)
+    view_orientation = rowan.from_matrix(args.viewport.viewMatrix[:, :3])
+    dp = freud.diffraction.DiffractionPattern(
+        grid_size=256,
+        output_size=256,
+    )
+    dp.compute(
+        system=data,
+        view_orientation=view_orientation,
+        zoom=1,
+        peak_width=1,
+    )
+    buf = dp.to_image(cmap="afmhot", vmax=np.max(dp.diffraction))
+    width, height, bytes_per_pixel = buf.shape
+    img = PySide2.QtGui.QImage(
+        buf,
+        width,
+        height,
+        width * bytes_per_pixel,
+        PySide2.QtGui.QImage.Format_RGBA8888,
+    )
+    # Paint QImage onto viewport canvas
+    args.painter.drawImage(10, 10, img)
