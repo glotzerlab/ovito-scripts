e5f52c34246c3e08f5f982a28f7dd1cb95531bfc
janbridley <bradleyjenj@gmail.com>
2024-10-21 20:27:04 -0400

    Update RDF overlay

--- freud/overlay_RDF.py
+++ freud/overlay_RDF.py
@@ -1,80 +1,68 @@
 # Copyright (c) 2019 The Regents of the University of Michigan
 # All rights reserved.
 # This software is licensed under the BSD 3-Clause License.
-
-import matplotlib
-import matplotlib.pyplot as plt
-import PySide6.QtGui
-
 import freud
+import matplotlib.pyplot as plt
+import numpy as np
+from ovito.data import DataCollection
+from ovito.vis import ViewportOverlayInterface
+from traits.api import Bool, Enum, Range, Tuple
 
-# Activate 'agg' backend for off-screen plotting.
-matplotlib.use("Agg")
 
+class RadialDistributionOverlay(ViewportOverlayInterface):
+    # Adjustable user parameters:
+    bins = Range(value=300, low=1, label="RDF Bins", dtype=int)
+    r_max = Range(value=5.0, low=0.0, label="Maximum Radius")
+    font_scale = Range(value=2.75, low=0.0, label="Font Scale")
 
-def render(
-    args,
-    bins: int = 300,
-    r_max: float = 5.0,
-    dpi: float = 150,
-    draw_x: float = 10,
-    width: float = None,
-    height: float = None,
-    draw_y: float = 10,
-    align: str = "bottom left",
-    compute_first_shell_min: bool = False,
-):
-    plt.close()
-    pipeline = args.scene.selected_pipeline
-    if not pipeline:
-        return
-    data = pipeline.compute(args.frame)
+    output_size = Tuple((512, 384), label="Output Image Size", dtype=tuple[int, int])
 
-    rdf = freud.density.RDF(bins=bins, r_max=r_max)
-    rdf.compute(data)
-
-    # Get size of rendered viewport image in pixels.
-
-    viewport_width = args.painter.window().width()
-    viewport_height = args.painter.window().height()
-    if width is None:
-        width = viewport_width / 600
-    if height is None:
-        height = viewport_height / 800
-    if "right" in align:
-        draw_x = viewport_width - dpi * width - draw_x
-    if "bottom" in align:
-        draw_y = viewport_height - dpi * height - draw_y
-
-    # Create figure
-    fig, ax = plt.subplots(figsize=(width, height), dpi=dpi)
-    rdf.plot(ax=ax)
-
-    if compute_first_shell_min:
-        import scipy
-
-        import numpy as np
-
-        rdf_minima = scipy.signal.argrelmin(rdf.rdf)[0]
-        min_point = [rdf.bin_centers[rdf_minima[np.argmin(rdf.rdf[rdf_minima])]]]
-        plt.vlines(
-            min_point, *ax.get_ylim(), "k", "dashed", label=min_point[0].round(3)
-        )
-        plt.legend()
-        print(f"RDF_MIN: {min_point}")
-
-    plt.tight_layout()
-    fig.patch.set_alpha(0.0)
-    plt.tight_layout()
-
-    # Render figure to an in-memory buffer.
-    buf = fig.canvas.print_to_buffer()
-
-    # Create a QImage from the memory buffer
-    res_x, res_y = buf[1]
-    img = PySide6.QtGui.QImage(
-        buf[0], res_x, res_y, PySide6.QtGui.QImage.Format_RGBA8888
+    anchor = Enum(
+        ["south west", "south east", "north west", "north east"],
+        value="north east",
+        label="Image Position",
     )
 
-    # Paint QImage onto rendered viewport
-    args.painter.drawImage(draw_x, draw_y, img)
+    compute_first_shell_min = Bool(value=False, label="Compute first shell radius")
+
+    cmap = "afmhot"
+
+    def render(
+        self,
+        canvas: ViewportOverlayInterface.Canvas,
+        data: DataCollection,
+        **kwargs,
+    ):
+        rdf = freud.density.RDF(bins=self.bins, r_max=self.r_max)
+        rdf.compute(data)
+
+        image_pos = (
+            0.01 if "west" in self.anchor else 0.99,
+            0.01 if "south" in self.anchor else 0.99,
+        )
+
+        size = self.output_size / np.array(canvas.logical_size)
+
+        # Create figure
+        with canvas.mpl_figure(
+            pos=image_pos,
+            size=size,
+            anchor=self.anchor,
+            tight_layout=True,
+            font_scale=self.font_scale,
+        ) as fig:
+            ax = fig.subplots()
+            rdf.plot(ax=ax)
+            ax.set_xticks(np.arange(rdf.bin_centers[0], self.r_max + 1).astype(int))
+            ax.set_yticks([])
+
+            if self.compute_first_shell_min:
+                import scipy
+
+                rdf_minima = scipy.signal.argrelmin(rdf.rdf)[0]
+                min_point = rdf.bin_centers[rdf_minima[np.argmin(rdf.rdf[rdf_minima])]]
+                plt.vlines(
+                    [min_point], *ax.get_ylim(), "k", "dashed", label=min_point.round(3)
+                )
+                plt.legend()
+                print(f"RDF_MIN: {min_point:.6f}")
